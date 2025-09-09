fd93988825eea649d92fcab7fd906dab655e5157
romulo-pll <romulo.lima@b2bpll.com.br>
2025-09-09 13:51:33 -0300

    chore: chenge route components

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route element={<LayoutMain />}>
           <Route index element={<PageHome />} />
-          <Route path="/componentes" element={<PageComponents />} />
+          <Route path="/components" element={<PageComponents />} />
         </Route>
       </Routes>
     </BrowserRouter>
